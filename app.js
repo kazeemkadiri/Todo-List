2da73903ff1e955c5a31cd621b105b9b697a4929
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2021-11-20 01:52:30 +0100

    Added code for the todo list routes

--- app.js
+++ app.js
@@ -2,12 +2,60 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const items = [];
+
+// Set view engine
+app.set("view engine", "ejs");
 
 // Setup middleware to parse the body of the request
 app.use(express.urlencoded({ extended: true }));
 
+// Setup the public path
+app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//   const today = new Date().getDay();
+//   let day = null;
+
+//   switch (today) {
+//     case 0:
+//       day = "Sunday";
+//       break;
+//     case 1:
+//       day = "Monday";
+//       break;
+//     case 2:
+//       day = "Tuesday";
+//       break;
+//     case 3:
+//       day = "Wednesday";
+//       break;
+//     case 4:
+//       day = "Thursday";
+//       break;
+//     case 5:
+//       day = "Friday";
+//       break;
+//     case 6:
+//       day = "Saturday";
+//       break;
+
+//     default:
+//       break;
+//   }
+//   res.render("index", { dayOfWeek: day });
+// });
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.render("list", { newItems: items });
+});
+
+app.post("/", (req, res) => {
+  const { todoItem } = req.body;
+
+  items.push(todoItem);
+
+  res.redirect("/");
 });
 
 app.listen(PORT, () => {
